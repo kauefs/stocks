32199b1ae6a2a32a52ee673f95868316910f98d5
KAUE <139394184+kauefs@users.noreply.github.com>
2024-05-02 07:20:00 -0300

    stocks.py

--- stocks.py
+++ stocks.py
@@ -17,7 +17,7 @@ st.sidebar.markdown('''
 [![Python](  https://img.shields.io/badge/-3-4584B6?logo=python&logoColor=FFDE57&labelColor=4584B6&color=646464)](https://www.python.org/)
 [![License]( https://img.shields.io/badge/License-Apache_2.0-D22128?logo=apache&logoColor=CB2138)](https://www.apache.org/licenses/LICENSE-2.0)
             ''')
-st.sidebar.text( '13 October 2023'       )
+st.sidebar.text(     '1 April 2024'      )
 st.sidebar.divider(                      )
 
 st.sidebar.header(   'Stocks'            )
