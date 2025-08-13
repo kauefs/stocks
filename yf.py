b2e1b22ea9d866434b191fc7890433cc55d92b54
KAUE <139394184+kauefs@users.noreply.github.com>
2025-08-13 20:11:12 -0300

    yf.py

--- yf.py
+++ yf.py
@@ -11,28 +11,60 @@ st.set_page_config(page_title='Stocks', page_icon='📊', layout='wide', initial
 st.sidebar.title    ('ƊⱭȾɅViƧi🧿Ƞ&trade;')
 st.sidebar.divider  (                     )
 st.sidebar.header   ('Stocks'             )
-# st.sidebar.success  ('Stocks'             )
 st.sidebar.subheader('Data Analysis'      )
-# st.sidebar.info     ('Data Analysis'      )
-# st.sidebar.write    ('Comparisson Charts' )
-st.sidebar.success  ('Comparisson Charts' )
+st.sidebar.success  ('Stock Analysis'     )
 st.sidebar.divider  (                     )
+ticker     =st.sidebar.text_input('Stock Ticker', 'FESA4' )
+SideBarInfo=st.sidebar.empty     (                        )
+Start          =(date.today(     )-timedelta(days= 365  ) )
+End            =(date.today(     )-timedelta(days=   1  ) )
+start          =st.sidebar.date_input(label='start', value=Start, format='YYYY.MM.DD')
+end            =st.sidebar.date_input(label= 'end' , value= End , format='YYYY.MM.DD')
+st.sidebar.divider (                                                        )
+st.sidebar.markdown('''Data from [Yahoo! Finance](https://finance.yahoo.com/)''')
+st.sidebar.markdown('''
+![2025.08.13   ](https://img.shields.io/badge/2025.08.13-000000)
 
+[![License     ](https://img.shields.io/badge/Apache--2.0-D22128?&logo=apache&logoColor=CB2138&label=License&labelColor=6D6E71)](https://www.apache.org/licenses/LICENSE-2.0)
 
-start ='2025-08-01'
-end   =date.today( ).strftime('%Y-%m-%d')
-st.title('Stock')
-stocks=st.multiselect('Select Stock Ticker',['VALE3','PETR4','BBAS3','BBSE3'], default=['VALE3'])
+[![GitHub      ](https://img.shields.io/badge/-000000?logo=github&logoColor=FFFFFF)](https://github.com/kauefs/)
+[![Medium      ](https://img.shields.io/badge/-000000?logo=medium&logoColor=FFFFFF)](https://medium.com/@kauefs)
+[![LinkedIn    ](https://img.shields.io/badge/in-0077B5?logo=linkedin&logoColor=FFFFFF)](https://www.linkedin.com/in/kauefs/)
+[![Python      ](https://img.shields.io/badge/3-646464?logo=python&logoColor=FFDE57&labelColor=4584B6)](https://www.python.org/)
+
+[![ƊⱭȾɅViƧi🧿Ƞ](https://img.shields.io/badge/ƊⱭȾɅViƧi🧿Ƞ&trade;-0065FF?style=plastic&logoColor=0065FF&label=&copy;2025&labelColor=0065FF)](https://datavision.one/)
+                    ''')
+# start ='2025-08-01'
+# end   =date.today( ).strftime('%Y-%m-%d')
+st.title('Stock Analysis')
+# stocks=st.multiselect('Select Stock Ticker',['VALE3','PETR4','BBAS3','BBSE3','ITUB3'], default=['VALE3'])
 @st.cache_data
-def LoadData               (tickers):
+def LoadData               (ticker):
     '''DownLoads Stocks Data from yFinance'''
-    if not tickers: return None
-    stock=[i+'.SA' for i in tickers]
+    if not ticker: return None
+    stock=[i+'.SA' for i in ticker]
     df=yf.download(stock, start=start, end=end, auto_adjust=True, rounding=True)
     df=df[['Open','High','Low','Close','Volume']].dropna( )
     return df
-with st.spinner('Loading Data…'):df=LoadData(stocks)
+with st.spinner('Loading Data…'):df=LoadData(ticker)
 if  df is not None and not df.empty:
-    st.subheader('Raw Data')
-    st.dataframe(df)
-else:st.warning ('No Data Found for Selected Tickers.')
+    # st.subheader('Raw Data')
+    # st.dataframe(df)
+    trace2={'x'       : df.index ,
+            'y'       : df.High  ,
+            'type'    : 'scatter',
+            'mode'    : 'lines'  ,
+            'line'    :{'width':2,                          'color':'#00FFFF'},
+            'showlegend': False  }
+    trace3={'x'       : df.index ,
+            'y'       : df.Low   ,
+            'type'    : 'scatter',
+            'mode'    : 'lines'  ,
+            'line'    :{'width':2,                          'color':'#808080'},
+            'showlegend': False  }
+    data  =   [trace2,trace3]
+    layout=go.Layout(xaxis_rangeslider_visible=False, paper_bgcolor='#FFFFFF', plot_bgcolor='#000000', title=stock[0])
+    fig   =go.Figure(data=data , layout=layout)
+    st.plotly_chart (fig, theme=  'streamlit' )
+else:st.warning     ('No Data Found for Selected Tickers.')
+st.divider          (                         )
