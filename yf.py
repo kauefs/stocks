6fc7c1e0957b4584afef19b7b9fb0ed8d49c16e4
KAUE <139394184+kauefs@users.noreply.github.com>
2025-08-13 19:20:20 -0300

    yf.py

--- yf.py
+++ yf.py
@@ -1,18 +1,20 @@
 import streamlit            as   st
 import yfinance             as   yf
 from   datetime           import date
-start='2025-08-01'
-end  =date.today( ).strftime('%Y-%m-%d')
+start ='2025-08-01'
+end   =date.today( ).strftime('%Y-%m-%d')
 st.title('Stock')
-stocks =['VALE3']
+stocks=st.multiselect('Select Stock Ticker',['VALE3','PETR4','BBAS3','BBSE3'], default=['VALE3'])
 @st.cache_data
-def LoadData               (stocks):
-    stock=[i+'.SA' for i in stocks]
+def LoadData               (tickers):
+    '''DownLoads Stocks Data from yFinance'''
+    if not tickers: return None
+    stock=[i+'.SA' for i in tickers]
     df=yf.download(stock, start=start, end=end, auto_adjust=True, rounding=True)
     df=df[['Open','High','Low','Close','Volume']].dropna( )
     return df
-state=st.text('Loading Data…')
-df   =LoadData        (stocks)
-state   .text('Loading Data… Done!')
-st.subheader (    'Raw Data' )
-st.dataframe (df.tail(     ) )
+with st.spinner('Loading Data…'):df=LoadData(stocks)
+if  df is not None and not df.empty:
+    st.subheader('Raw Data')
+    st.dataframe(df)
+else:st.warning ('No Data Found for Selected Tickers.')
