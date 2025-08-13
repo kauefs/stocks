fd403a2324fdcac8e13bb3a7b1b4072388078286
KAUE <139394184+kauefs@users.noreply.github.com>
2025-08-13 18:37:53 -0300

    yf.py

--- yf.py
+++ yf.py
@@ -1,16 +1,18 @@
 import streamlit            as   st
 import yfinance             as   yf
 from   datetime           import date
-START='2025-08-01'
-TODAY=date.today( ).strftime('%Y-%m-%d')
+start='2025-08-01'
+end  =date.today( ).strftime('%Y-%m-%d')
 st.title('Stock')
-stocks =('VALE3.SA')
+stocks =['VALE3']
 @st.cache_data
-def LoadData        (ticker):
-    data=yf.download(ticker, START, TODAY, auto_adjust=True, rounding=True)
-    return data
+def LoadData               (stocks):
+    stock=[i+'.SA' for i in stocks]
+    df=yf.download(stock, start=start, end=end, auto_adjust=True, rounding=True)
+    df=df[['Open','High','Low','Close','Volume']].dropna( )
+    return df
 state=st.text('Loading Data…')
-data=LoadData       (stocks )
+df   =LoadData        (stocks)
 state   .text('Loading Data… Done!')
-st.subheader (    'Raw Data')
-st.dataframe (data.tail(   ))
+st.subheader (    'Raw Data' )
+st.dataframe (df.tail(     ) )
