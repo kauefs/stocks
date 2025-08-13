4f152c14925650d362fee0c7e8f84823083a6341
KAUE <139394184+kauefs@users.noreply.github.com>
2025-08-13 12:31:36 -0300

    stocks.py

--- stocks.py
+++ stocks.py
@@ -6,44 +6,43 @@ import plotly.graph_objects  as go
 from   plotly.subplots   import make_subplots
 from       ta.volatility import BollingerBands
 from          datetime   import date, timedelta
-
 st.set_page_config(page_title='Stocks', page_icon='📊', layout='wide', initial_sidebar_state='expanded')
-
 # SIDE:
 st.sidebar.title    ('ƊⱭȾɅViƧi🧿Ƞ&trade;')
 st.sidebar.divider  (                     )
-
-st.sidebar.header   ('Stocks'             )
-st.sidebar.subheader('Data Analysis'      )
+# st.sidebar.header   ('Stocks'           )
+st.sidebar.success  ('Stocks'             )
+# st.sidebar.subheader('Data Analysis'    )
+st.sidebar.info     ('Data Analysis'      )
 st.sidebar.write    ('Comparisson Charts' )
 st.sidebar.divider  (                     )
 
-stock1         = st.sidebar.text_input('Yahoo! Stock Ticker 1:','BBAS3.SA')
-SideBarInfo1   = st.sidebar.empty(                                        )
+stock1         =st.sidebar.text_input('Yahoo! Stock Ticker 1:','BBAS3.SA')
+SideBarInfo1   =st.sidebar.empty     (                                   )
 
-stock2         = st.sidebar.text_input('Yahoo! Stock Ticker 2:','BBSE3.SA')
-SideBarInfo2   = st.sidebar.empty(                                        )
+stock2         =st.sidebar.text_input('Yahoo! Stock Ticker 2:','BBSE3.SA')
+SideBarInfo2   =st.sidebar.empty     (                                   )
 
-Start          = (date.today(  )-timedelta(days=150))
-End            = (date.today(  )-timedelta(days=  1))
-start          = st.sidebar.date_input(label='From:', value=Start, format='YYYY.MM.DD')
-end            = st.sidebar.date_input(label='To:'  , value=End  , format='YYYY.MM.DD')
+Start          =(date.today(  )-timedelta(days=150))
+End            =(date.today(  )-timedelta(days=  1))
+start          =st.sidebar.date_input(label='From:', value=Start, format='YYYY.MM.DD')
+end            =st.sidebar.date_input(label= 'To:' , value= End , format='YYYY.MM.DD')
 
-df1            = yf.download(stock1, start=start, end=end, prepost=False, auto_adjust=False, actions=False, rounding=True, multi_level_index=False)
+df1            =yf.download(stock1, start=start, end=end, prepost=False, auto_adjust=False, actions=False, rounding=True, multi_level_index=False)
 df1.reset_index(inplace=True)
-df1['Date']    = pd.to_datetime(df1['Date'], format='%Y-%m-%d').dt.date
-FilteredDF1    = df1.loc[(df1['Date'] >= start)&(df1['Date']  <= end)]
+df1['Date']    =pd.to_datetime(df1['Date'], format='%Y-%m-%d').dt.date
+FilteredDF1    =df1.loc[(df1['Date'] >= start)&(df1['Date']  <= end)]
 Stock1         = (FilteredDF1['High']  + FilteredDF1['Low'])/2
 SideBarInfo1.info('{} entries for {}'.format(Stock1.shape[0], stock1))
 
-df2            = yf.download(stock2, start=start, end=end, prepost=False, auto_adjust=False, actions=False, rounding=True, multi_level_index=False)
+df2            =yf.download(stock2, start=start, end=end, prepost=False, auto_adjust=False, actions=False, rounding=True, multi_level_index=False)
 df2.reset_index(inplace=True)
-df2['Date']    = pd.to_datetime(df2['Date'], format='%Y-%m-%d').dt.date
-FilteredDF2    = df2.loc[(df2['Date'] >= start)&(df2['Date']  <= end)]
+df2['Date']    =pd.to_datetime(df2['Date'], format='%Y-%m-%d').dt.date
+FilteredDF2    =df2.loc[(df2['Date'] >= start)&(df2['Date']  <= end)]
 Stock2         = (FilteredDF2['High']  + FilteredDF2['Low'])/2
 SideBarInfo2.info  ('{} entries for {}'.format(Stock2.shape[0], stock2))
 
-st.sidebar.divider (                                                          )
+st.sidebar.divider (                                                        )
 st.sidebar.markdown('''Data: [Yahoo! Finance](https://finance.yahoo.com/)''')
 st.sidebar.markdown('''
 ![2024.04.01   ](https://img.shields.io/badge/2024.04.01-000000)
@@ -57,22 +56,20 @@ st.sidebar.markdown('''
 
 [![ƊⱭȾɅViƧi🧿Ƞ](https://img.shields.io/badge/ƊⱭȾɅViƧi🧿Ƞ&trade;-0065FF?style=plastic&logoColor=0065FF&label=&copy;2025&labelColor=0065FF)](https://datavision.one/)
                     ''')
-
 # MAIN:
 st.divider  (                       )
 st.title    ('STOCKS'               )
 st.divider  (                       )
 st.subheader('Comparisson Charts'   )
+
 st.markdown (f'''➡️ **{stock1}**:''')
-
-close1=df1['Close']= df1['Close'].squeeze()
+close1=df1['Close']=df1['Close'].squeeze( )
 bb = BollingerBands(close=close1, window=15, window_dev=2)
-df1['BBH' ] = bb.bollinger_hband()
-df1['BBL' ] = bb.bollinger_lband()
-df1['MA15'] = df1['Close'].rolling(window=15).mean()
-
-fig = make_subplots(rows=  2, cols  =  1, shared_xaxes=True,
-                    vertical_spacing=.05,
+df1['BBH' ]=bb.bollinger_hband( )
+df1['BBL' ]=bb.bollinger_lband( )
+df1['MA15']=df1['Close'].rolling(window=15).mean( )
+fig=make_subplots(rows=2, cols=1, shared_xaxes=True,
+                    vertical_spacing= .05,
                     subplot_titles  =('Price','Volume'),
                     row_width       =[.25,.75])
 fig.add_trace(go.Candlestick(x      =df1['Date' ],
@@ -81,42 +78,40 @@ fig.add_trace(go.Candlestick(x      =df1['Date' ],
                              low    =df1['Low'  ],
                              close  =df1['Close'],
                              name   =    'CandleStick'),
-                             row    =1, col=1)
+                             row    =  1, col =1)
 fig.add_trace(go.Scatter(x          =df1['Date' ],
                          y          =df1['BBH'  ],
                          mode       =    'lines',
                          name       =    'BBH – Bollinger Higher Band'),
-                         row        =1, col=1)
+                         row        =  1, col =1)
 fig.add_trace(go.Scatter(x          =df1['Date' ],
                          y          =df1['MA15' ],
                          mode       =    'lines',
                          name       =    'MA15 – Moving Average 15 Days'),
-                         row        =1, col=1)
+                         row        =  1, col =1)
 fig.add_trace(go.Scatter(x          =df1['Date' ],
                          y          =df1['BBL'  ],
                          mode       =    'lines',
                          name       =    'BBL – Bollinger Lower Band'),
-                         row        =1, col=1)
+                         row        =  1, col =1 )
 fig.add_trace(go.Bar(x              =df1['Date'  ],
                      y              =df1['Volume'],
                      name           =    'Volume'),
-                     row            =2, col=1)
+                     row            =  2, col =1 )
 fig.update_layout(#yaxis_title       =    'Price',
                   xaxis_rangeslider_visible= False,
-                  width             =1000,  height=555)
+                  width            = 1000 , height=555)
 st.plotly_chart(fig, theme='streamlit')
-st.divider(                           )
+st.divider     (                      )
 
 st.markdown(f'''➡️ **{stock2}**:''')
-
-close2=df2['Close']= df2['Close'].squeeze()
+close2=df2['Close']=df2['Close'].squeeze( )
 bb = BollingerBands(close=close2, window=15, window_dev=2)
-df2['BBH' ] = bb.bollinger_hband()
-df2['BBL' ] = bb.bollinger_lband()
-df2['MA15'] = df2['Close'].rolling(window=15).mean()
-
-fig = make_subplots(rows=  2, cols  = 1, shared_xaxes=True,
-                    vertical_spacing=.05,
+df2['BBH' ]=bb.bollinger_hband( )
+df2['BBL' ]=bb.bollinger_lband( )
+df2['MA15']=df2['Close'].rolling(window=15).mean( )
+fig = make_subplots(rows=2, cols=1, shared_xaxes=True,
+                    vertical_spacing= .05,
                     subplot_titles  =('Price','Volume'),
                     row_width       =[.25,.75])
 fig.add_trace(go.Candlestick(x      =df2['Date' ],
@@ -125,28 +120,28 @@ fig.add_trace(go.Candlestick(x      =df2['Date' ],
                              low    =df2['Low'  ],
                              close  =df2['Close'],
                              name   =    'CandleStick'),
-                             row    =1, col=1)
+                             row    =  1, col =1)
 fig.add_trace(go.Scatter(x          =df2['Date' ],
                          y          =df2['BBH'  ],
                          mode       =    'lines',
                          name       =    'BBH – Bollinger Higher Band'),
-                         row        =1, col=1)
+                         row        =  1, col =1)
 fig.add_trace(go.Scatter(x          =df2['Date' ],
                          y          =df2['MA15' ],
                          mode       =    'lines',
                          name       =    'MA15 – Moving Average 15 Days'),
-                         row        =1, col=1)
+                         row        =  1, col =1)
 fig.add_trace(go.Scatter(x          =df2['Date' ],
                          y          =df2['BBL'  ],
                          mode       =    'lines',
                          name       =    'BBL – Bollinger Lower Band'),
-                         row        =1, col=1)
+                         row        =  1, col = 1)
 fig.add_trace(go.Bar(x              =df2['Date'  ],
                      y              =df2['Volume'],
                      name           =    'Volume'),
-                     row            =2, col=1)
+                     row            =  2, col = 1)
 fig.update_layout(#yaxis_title       =    'Price',
                   xaxis_rangeslider_visible= False,
-                  width             =1000,   height=555)
+                  width            = 1000 , height=555)
 st.plotly_chart(fig, theme='streamlit')
-st.divider(                           )
+st.divider     (                      )
