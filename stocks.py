3412e5c7b0b78bc1fce63d4ac29b8c3320395aae
KAUE <139394184+kauefs@users.noreply.github.com>
2025-08-14 11:45:28 -0300

    stocks.py

--- stocks.py
+++ stocks.py
@@ -48,14 +48,14 @@ st.sidebar.markdown('''Data from [Yahoo! Finance](https://finance.yahoo.com/)'''
 st.sidebar.markdown('''
 ![2024.04.01   ](https://img.shields.io/badge/2024.04.01-000000)
 
+![2025.08.15   ](https://img.shields.io/badge/2025.08.15-000000)
+
 [![License     ](https://img.shields.io/badge/Apache--2.0-D22128?&logo=apache&logoColor=CB2138&label=License&labelColor=6D6E71)](https://www.apache.org/licenses/LICENSE-2.0)
 
 [![GitHub      ](https://img.shields.io/badge/-000000?logo=github&logoColor=FFFFFF)](https://github.com/kauefs/)
 [![Medium      ](https://img.shields.io/badge/-000000?logo=medium&logoColor=FFFFFF)](https://medium.com/@kauefs)
 [![LinkedIn    ](https://img.shields.io/badge/in-0077B5?logo=linkedin&logoColor=FFFFFF)](https://www.linkedin.com/in/kauefs/)
 [![Python      ](https://img.shields.io/badge/3-646464?logo=python&logoColor=FFDE57&labelColor=4584B6)](https://www.python.org/)
-
-![2025.08.15   ](https://img.shields.io/badge/2025.08.15-000000)
 
 [![ƊⱭȾɅViƧi🧿Ƞ](https://img.shields.io/badge/ƊⱭȾɅViƧi🧿Ƞ&trade;-0065FF?style=plastic&logoColor=0065FF&label=&copy;2025&labelColor=0065FF)](https://datavision.one/)
                     ''')
